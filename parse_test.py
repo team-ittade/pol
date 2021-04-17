ad5591da1fa63d74753356f468f39df5332d763c
Diego Alloza González <sralloza@gmail.com>
2021-04-17 23:09:15 +0200

    chore: add docs in parse-test script

--- parse_test.py
+++ parse_test.py
@@ -2,18 +2,25 @@ from pathlib import Path
 import re
 import typer
 
+Arg = typer.Argument
+Opt = typer.Option
+
 app = typer.Typer(add_completion=False)
 regex = r"([\w. :áéíóúÁÉÍÓÚ¿?,ñÑ%+\"“”-]+)\nSeleccione una:\na. ([\w. :áéíóúÁÉÍÓÚ¿?,ñÑ%+\"“”-]+)\nb. ([\w. :áéíóúÁÉÍÓÚ¿?,ñÑ%+\"“”-]+)\nc. ([\w. :áéíóúÁÉÍÓÚ¿?,ñÑ%+\"“”-]+)\nd. ([\w. :áéíóúÁÉÍÓÚ¿?,ñÑ%+\"“”-]+)\nRetroalimentación\nLa respuesta correcta es: ([\w. :áéíóúÁÉÍÓÚ¿?,ñÑ%+\"“”-]+)"
 
 
 @app.command()
-def main(tema: int, file: str = typer.Argument("test.txt", exists=True)):
-    output_path = Path(__file__).with_name("docs") / f"tests/tema-{tema}.md"
+def main(
+    lection: int = Opt(..., help="Lection of the test"),
+    file: str = Arg("test.txt", exists=True, help="Path to the test's text"),
+):
+
+    output_path = Path(__file__).with_name("docs") / f"tests/tema-{lection}.md"
     data = Path(file).read_text("utf8")
 
     matches = re.finditer(regex, data)
     with output_path.open("wt", encoding="utf8") as fh:
-        fh.write(f"# Test tema {tema}\n")
+        fh.write(f"# Test tema {lection}\n")
         for match in matches:
             q, a, b, c, d, sol = match.groups()
             a_sol = b_sol = c_sol = d_sol = " "
